7cfc730e2323d20ac6fbf1e6fbb529daa4b8e593
suzzang <soochoi0325@gmail.com>
2018-07-03 00:01:52 +0900

    내상품 - 찜한 상품 탭 구현 완료, 장바구니 구현중

--- Woong_Android/app/src/main/java/com/woong/woong_android/myproduct/MyProduct.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/myproduct/MyProduct.kt
@@ -2,14 +2,20 @@ package com.woong.woong_android.myproduct
 
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.woong.woong_android.R
 import com.woong.woong_android.myproduct.adapter.MpPagerAdapter
+import com.woong.woong_android.myproduct.adapter.MyProductAdapter
+import com.woong.woong_android.myproduct.data.MyProductBookmarkData
+import kotlinx.android.synthetic.main.fragment_myproduct_bookmark.view.*
 import kotlinx.android.synthetic.main.fragment_myproduct_tab.view.*
 
 class MyProduct :android.support.v4.app.Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_myproduct_tab,container,false)
         val myProductPagerAdapter = MpPagerAdapter(activity!!.supportFragmentManager)
@@ -19,6 +25,10 @@ class MyProduct :android.support.v4.app.Fragment() {
         viewPager.adapter = myProductPagerAdapter
         tabLayout.setTabTextColors(Color.parseColor("#b2b2b2"),Color.parseColor("#227958"))
         tabLayout.setupWithViewPager(viewPager)
+
+
+
+
 
         return v
     }
