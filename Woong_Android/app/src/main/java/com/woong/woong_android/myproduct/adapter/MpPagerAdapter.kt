adfdde62b0724578241756b1dbfd1011b0421313
suzzang <soochoi0325@gmail.com>
2018-07-02 21:20:50 +0900

    하단탭 - 내상품 : 상단바 뷰페이저 구현(찜한 상품, 장바구니), 찜한 상품 탭  리사이클러뷰 구현 중

---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/myproduct/adapter/MpPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.woong.woong_android.myproduct.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.woong.woong_android.myproduct.bookmark.MyProductBookMark
+import com.woong.woong_android.myproduct.cart.MyProductCart
+
+class MpPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment? {
+        when(position){
+            0 -> return MyProductBookMark()
+            1 -> return MyProductCart()
+        }
+        return null
+    }
+
+    override fun getCount(): Int {
+        return 2 //페이지 개수 (내상품 - 찜한상품, 장바구니)
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position){
+            0 -> return "찜한 상품"
+            1 -> return "장바구니"
+        }
+        return null
+    }
+}
---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/myproduct/adapter/MyProductAdapter.kt
@@ -0,0 +1,19 @@
+package com.woong.woong_android.myproduct.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.woong.woong_android.myproduct.viewholder.MyProductViewHolder
+
+class MyProductAdapter : RecyclerView.Adapter<MyProductViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyProductViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: MyProductViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
