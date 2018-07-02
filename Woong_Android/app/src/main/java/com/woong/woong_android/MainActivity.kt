b0024e14abb64796bc6f341f07afe25f4d665df8
sovovy <sovovy@gmail.com>
2018-07-02 23:12:44 +0900

    Add SubMenu Layout, Class kt

--- Woong_Android/app/src/main/java/com/woong/woong_android/MainActivity.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/MainActivity.kt
@@ -3,13 +3,22 @@ package com.woong.woong_android
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.SubMenu
 import android.view.View
+import com.woong.woong_android.R
+import com.woong.woong_android.home.main.HomeMain
 import com.woong.woong_android.myproduct.MyProduct
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(p0: View?) {
         when(p0){
+            btn_home_main ->{
+                addFragment(HomeMain())
+            }
+            btn_market_main ->{
+                addFragment(com.woong.woong_android.home.submenu.SubMenu())
+            }
             btn_myproduct_main ->{
                 addFragment(MyProduct())
             }
@@ -19,7 +28,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addFragment(HomeMain())
+
         btn_myproduct_main.setOnClickListener(this)
+        btn_home_main.setOnClickListener(this)
+        btn_market_main.setOnClickListener(this)
 
     }
 
