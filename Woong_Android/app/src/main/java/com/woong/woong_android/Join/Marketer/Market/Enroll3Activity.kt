3cf0b93fd8b88e61778fff03f76c3d9a19b75921
HyeranShin <hyebaragi__@naver.com>
2018-07-05 21:50:14 +0900

    판매자 뷰 구현중 커밋

---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/Join/Marketer/Market/Enroll3Activity.kt
@@ -0,0 +1,13 @@
+package com.woong.woong_android.Join.Marketer.Market
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.woong.woong_android.R
+
+class Enroll3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market_enroll3)
+    }
+}
