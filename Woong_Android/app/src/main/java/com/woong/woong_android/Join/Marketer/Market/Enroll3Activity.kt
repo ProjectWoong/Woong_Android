5c536532de83fb7f03e4789440aaca839b69a375
HyeranShin <hyebaragi__@naver.com>
2018-07-06 11:59:24 +0900

    마켓 등록 뷰 구현, 액티비티 연결 완료

--- Woong_Android/app/src/main/java/com/woong/woong_android/Join/Marketer/Market/Enroll3Activity.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/Join/Marketer/Market/Enroll3Activity.kt
@@ -1,13 +1,20 @@
 package com.woong.woong_android.Join.Marketer.Market
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.woong.woong_android.Login.LoginActivity
 import com.woong.woong_android.R
+import kotlinx.android.synthetic.main.activity_market_enroll3.*
 
 class Enroll3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_market_enroll3)
+        btn_ok_enroll3.setOnClickListener {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
