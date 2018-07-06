4742cedc779933545aebb3b201d6a07ae76ed041
sovovy <sovovy@gmail.com>
2018-07-07 01:29:29 +0900

    <판매자 마켓> 레이아웃 기초 작성

---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/seller_market/adapter/SmPagerAdapter.kt
@@ -0,0 +1,35 @@
+package com.woong.woong_android.seller_market.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.woong.woong_android.seller_market.album.SellerMarketAlbum
+import com.woong.woong_android.seller_market.intro.SellerMarketIntro
+import com.woong.woong_android.seller_market.product.SellerMarketProduct
+import com.woong.woong_android.seller_market.review.SellerMarketReview
+
+class SmPagerAdapter (fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0 -> return SellerMarketIntro()
+            1 -> return SellerMarketProduct()
+            2 -> return SellerMarketAlbum()
+            3 -> return SellerMarketReview()
+        }
+        return SellerMarketIntro()
+    }
+
+    override fun getCount(): Int {
+        return 4
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when(position){
+            0 -> return "소개"
+            1 -> return "물품"
+            2 -> return "앨범"
+            3 -> return "후기"
+        }
+        return null
+    }
+}
---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/seller_market/intro/SellerMarketIntro.kt
@@ -0,0 +1,16 @@
+package com.woong.woong_android.seller_market.intro
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woong.woong_android.R
+
+class SellerMarketIntro: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_sellermarket_intro,container,false)
+        return v
+    }
+}
