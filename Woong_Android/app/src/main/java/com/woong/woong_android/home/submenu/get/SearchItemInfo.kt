5932ca8c7fd63ff0e403e4710e8ab7cb69100a2d
sovovy <sovovy@gmail.com>
2018-07-12 05:19:07 +0900

    <물품 검색> 완성

---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/home/submenu/get/SearchItemInfo.kt
@@ -0,0 +1,5 @@
+package com.woong.woong_android.home.submenu.get
+
+data class SearchItemInfo (
+        var item_info:ArrayList<GetSearchItemResponseData>
+)
--- Woong_Android/app/src/main/java/com/woong/woong_android/market/Market.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/market/Market.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.woong.woong_android.R
 import com.woong.woong_android.market.adapter.MarketPagerAdpter
-import com.woong.woong_android.market_usertoken
+import com.woong.woong_android.woong_usertoken
 import kotlinx.android.synthetic.main.fragment_market_tab.view.*
 
 class Market :android.support.v4.app.Fragment() {
@@ -15,7 +15,7 @@ class Market :android.support.v4.app.Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_market_tab ,container,false)
         var user_token = arguments!!.getString("user_token")
-        market_usertoken.user_token = user_token
+        woong_usertoken.user_token = user_token
         val myProductPagerAdapter = MarketPagerAdpter(this.childFragmentManager) // 프래그먼트안에 뷰페이저 쓸경우 childFragmentManager써주세욤
         val viewPager = v.viewpager_market
         val tabLayout = v.tab_top_market
