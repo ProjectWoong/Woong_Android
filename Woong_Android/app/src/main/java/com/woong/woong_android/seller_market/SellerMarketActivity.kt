b0e0a3f8f70aabb26b0fa51f2ac1efcff9e389dc
sovovy <sovovy@gmail.com>
2018-07-11 18:08:26 +0900

    <중분류-마켓의 물품상세> 연결 완료

--- Woong_Android/app/src/main/java/com/woong/woong_android/seller_market/SellerMarketActivity.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/seller_market/SellerMarketActivity.kt
@@ -6,15 +6,26 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
+import android.support.v4.app.Fragment
 import android.support.v7.app.ActionBar
 import android.view.View
 import com.woong.woong_android.R
+import com.woong.woong_android.home.submenu.product.SellerIdx
 import com.woong.woong_android.seller_market.adapter.SmPagerAdapter
+import com.woong.woong_android.seller_market.product.SellerMarketProductDetail
 import kotlinx.android.synthetic.main.activity_sellermarket.*
 
 
 
 class SellerMarketActivity : AppCompatActivity() {
+
+    fun replaceFragment(fragment: Fragment) {
+        val fm = supportFragmentManager
+        val transaction = fm.beginTransaction()
+        transaction.replace(R.id.frame_sellermarket,fragment)
+        transaction.addToBackStack(null)    // 이전 상태를 백스택에 추가하여 사용자가 백버튼을 눌렀을때에 대한 호환성 추가
+        transaction.commit()
+    }
 
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +37,13 @@ class SellerMarketActivity : AppCompatActivity() {
         val tabLayout = tab_top_sellermarket
 
         viewPager.adapter = myProductPagerAdapter
+        viewPager.currentItem = SellerIdx.id
         tabLayout.setTabTextColors(Color.parseColor("#ffffff"), Color.parseColor("#ffffff"))
         tabLayout.setupWithViewPager(viewPager)
+        if(SellerIdx.id==1){
+            replaceFragment(SellerMarketProductDetail())
+            SellerIdx.id=0
+        }
 
         setSupportActionBar(toolbar)
 
