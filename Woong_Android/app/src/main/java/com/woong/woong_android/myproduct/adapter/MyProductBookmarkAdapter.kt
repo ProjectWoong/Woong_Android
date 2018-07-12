af8c0db2ee90b98a6c8f366e80cc34d512fa8383
sovovy <sovovy@gmail.com>
2018-07-13 04:38:39 +0900

    <찜한 목록> 찜하기 버튼 활성화. 물품 누르면 상세 페이지 전환.

--- Woong_Android/app/src/main/java/com/woong/woong_android/myproduct/adapter/MyProductBookmarkAdapter.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/myproduct/adapter/MyProductBookmarkAdapter.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.RequestManager
 import com.woong.woong_android.R
+import com.woong.woong_android.applicationcontroller.ApplicationController
+import com.woong.woong_android.home.post.PostFavoriteResponse
 import com.woong.woong_android.myproduct.bookmark.MyProductBookMark
 import com.woong.woong_android.myproduct.get.GetFavoriteResponseData
 import com.woong.woong_android.myproduct.viewholder.MyProductBookmarkViewHolder
+import com.woong.woong_android.woong_usertoken
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MyProductBookmarkAdapter(private var bookmarkItems : ArrayList<GetFavoriteResponseData>,var requestManager: RequestManager) : RecyclerView.Adapter<MyProductBookmarkViewHolder>() {
 
@@ -27,6 +33,8 @@ class MyProductBookmarkAdapter(private var bookmarkItems : ArrayList<GetFavorite
     override fun getItemCount(): Int = bookmarkItems.size
 
     override fun onBindViewHolder(bookmarkViewHolder: MyProductBookmarkViewHolder, position: Int) {
+        var networkService = ApplicationController.instance.networkService
+
         requestManager.load(bookmarkItems[position].file_key).into(bookmarkViewHolder.productimg)
         bookmarkViewHolder.marketname.text = bookmarkItems[position].market_name
         bookmarkViewHolder.productname.text = bookmarkItems[position].item_name
@@ -41,5 +49,34 @@ class MyProductBookmarkAdapter(private var bookmarkItems : ArrayList<GetFavorite
             bookmarkViewHolder.secondtag.visibility = View.GONE
         else
             bookmarkViewHolder.secondtag.visibility = View.VISIBLE
+        if (bookmarkItems[position].favorite_flag==1){
+            bookmarkViewHolder.favorite.setImageResource(R.drawable.home_select_category_like1)
+            bookmarkViewHolder.favorite.setOnClickListener {
+                val delFavorite = networkService.delFavorite(woong_usertoken.user_token, bookmarkItems[position].item_id)
+                delFavorite.enqueue(object : Callback<PostFavoriteResponse> {
+                    override fun onFailure(call: Call<PostFavoriteResponse>?, t: Throwable?) {
+                    }
+                    override fun onResponse(call: Call<PostFavoriteResponse>?, response: Response<PostFavoriteResponse>?) {
+                        if (response!!.isSuccessful) {
+                            bookmarkViewHolder.favorite.setImageResource(R.drawable.home_select_category_no_like)
+                        }
+                    }
+                })
+            }
+        }else{
+            bookmarkViewHolder.favorite.setImageResource(R.drawable.home_select_category_no_like)
+            bookmarkViewHolder.favorite.setOnClickListener {
+                val postFavorite = networkService.postFavorite(woong_usertoken.user_token, bookmarkItems[position].item_id)
+                postFavorite.enqueue(object : Callback<PostFavoriteResponse> {
+                    override fun onFailure(call: Call<PostFavoriteResponse>?, t: Throwable?) {
+                    }
+                    override fun onResponse(call: Call<PostFavoriteResponse>?, response: Response<PostFavoriteResponse>?) {
+                        if (response!!.isSuccessful) {
+                            bookmarkViewHolder.favorite.setImageResource(R.drawable.home_select_category_like1)
+                        }
+                    }
+                })
+            }
+        }
     }
 }
