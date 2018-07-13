d48b843931badacbbf6016afbacafce84fc87bc6
suzzang <soochoi0325@gmail.com>
2018-07-13 17:16:04 +0900

    처음로그인시 위치등록, 로그인후 위치가져오기, 위치변경하기 완료

---
+++ Woong_Android/app/src/main/java/com/woong/woong_android/map/get/GetLocationResponseData.kt
@@ -0,0 +1,5 @@
+package com.woong.woong_android.map.get
+
+data class GetLocationResponseData  (
+       var real_address:String?
+)
--- Woong_Android/app/src/main/java/com/woong/woong_android/map/location_change/LocationSearchResult.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/map/location_change/LocationSearchResult.kt
@@ -10,12 +10,19 @@ import android.view.View
 import android.view.ViewGroup
 import com.woong.woong_android.MainActivity
 import com.woong.woong_android.R
+import com.woong.woong_android.applicationcontroller.ApplicationController
 import com.woong.woong_android.home.main.HomeMain
+import com.woong.woong_android.home.main.Idx.idx
+import com.woong.woong_android.location
 import com.woong.woong_android.map.MapActivity
 import com.woong.woong_android.map.get.GetLocationListResponse
 import com.woong.woong_android.map.get.GetLocationListResponseData
+import com.woong.woong_android.map.get.GetLocationResponse
 import com.woong.woong_android.map.location_register.LocationSearchAdapter
+import com.woong.woong_android.map.put.PutLocationRegisterResponse
+import com.woong.woong_android.map.put.PutLocationRegisterResponseData
 import com.woong.woong_android.network.NetworkService
+import com.woong.woong_android.woong_usertoken
 import kotlinx.android.synthetic.main.activity_location_search.*
 import kotlinx.android.synthetic.main.fragment_locationsearch_result.view.*
 import retrofit2.Call
@@ -28,11 +35,38 @@ import kotlin.math.log
 class LocationSearchResult() : android.support.v4.app.Fragment(),View.OnClickListener {
     override fun onClick(p0: View?) {
         val idx : Int = rv_result_consumer_location_search.getChildAdapterPosition(p0)
-        val intent = Intent(context, MainActivity::class.java)
-        intent.putExtra("search_address",locationSearchItems[idx].address_name)
-        intent.putExtra("address_flag",1)
-        Log.v("주소",locationSearchItems[idx].address_name)
-        startActivity(intent)
+        networkService = ApplicationController.instance.networkService
+        var usertoken = woong_usertoken.user_token
+        var longitude = locationSearchItems[idx].x!!.toDouble()
+        var latitude = locationSearchItems[idx].y!!.toDouble()
+        var real_address = locationSearchItems[idx].address_name
+
+        val putLocationRegisterResponseData = PutLocationRegisterResponseData(latitude,longitude,real_address)
+        val putChangeLocation = networkService.putLocationRegister(usertoken,putLocationRegisterResponseData)
+        putChangeLocation.enqueue(object:Callback<PutLocationRegisterResponse>{
+            override fun onFailure(call: Call<PutLocationRegisterResponse>?, t: Throwable?) {
+
+            }
+
+            override fun onResponse(call: Call<PutLocationRegisterResponse>?, response: Response<PutLocationRegisterResponse>?) {
+                if(response!!.isSuccessful){
+                    val intent = Intent(context, MainActivity::class.java)
+                   // intent.putExtra("search_address",locationSearchItems[idx].address_name)
+                    intent.putExtra("address_flag",1)
+                    //Log.v("주소",locationSearchItems[idx].address_name)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    startActivity(intent)
+
+                }
+            }
+
+        })
+
+
+
+
+
+
 
 
     }
@@ -63,6 +97,7 @@ class LocationSearchResult() : android.support.v4.app.Fragment(),View.OnClickLis
 
             override fun onResponse(call: Call<GetLocationListResponse>?, response: Response<GetLocationListResponse>?) {
                 if(response!!.isSuccessful){
+
                     locationSearchItems = response.body().documents
                     locationSearchAdapter = LocationSearchAdapter(locationSearchItems)
 
