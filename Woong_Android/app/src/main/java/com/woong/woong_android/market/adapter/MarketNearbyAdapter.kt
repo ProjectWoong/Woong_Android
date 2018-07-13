852237718c56faa931cd7c8d4a134264ecdb4a43
sovovy <sovovy@gmail.com>
2018-07-13 21:59:38 +0900

    <마켓탭 - 내 주변 마켓> 아이템 누르면 마켓으로 이동. 뱃지 표시 완료.

--- Woong_Android/app/src/main/java/com/woong/woong_android/market/adapter/MarketNearbyAdapter.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/market/adapter/MarketNearbyAdapter.kt
@@ -2,26 +2,56 @@ package com.woong.woong_android.market.adapter
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.bumptech.glide.RequestManager
 import com.woong.woong_android.R
+import com.woong.woong_android.applicationcontroller.ApplicationController
 import com.woong.woong_android.market.viewholder.MarketNearbyViewHolder
+import com.woong.woong_android.seller_market.get.GetBookmarkFlagResponse
 import com.woong.woong_android.seller_market.get.GetNearMarketListResponseData
+import com.woong.woong_android.woong_usertoken
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MarketNearbyAdapter(private var nearbyItems : ArrayList<GetNearMarketListResponseData>,var requestManager: RequestManager) : RecyclerView.Adapter<MarketNearbyViewHolder>() {
 
+    private lateinit var onItemClick : View.OnClickListener
+
+    fun setOnItemClickListener(l : View.OnClickListener){
+        onItemClick = l
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarketNearbyViewHolder {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.item_market_mymarket,parent,false)
+        view.setOnClickListener(onItemClick)
         return MarketNearbyViewHolder(view)
     }
 
     override fun getItemCount(): Int = nearbyItems.size
 
     override fun onBindViewHolder(nearbyViewHolder: MarketNearbyViewHolder, position: Int) {
+        val networkService = ApplicationController.instance.networkService
+
         requestManager.load(nearbyItems[position].title_image_key).into(nearbyViewHolder.img)
         nearbyViewHolder.name.text = nearbyItems[position].market_name
         nearbyViewHolder.address.text = nearbyItems[position].market_address
+
+        var getBookmarkFlag = networkService.getBookmarkFlag(woong_usertoken.user_token, nearbyItems[position].market_id)
+        getBookmarkFlag.enqueue(object : Callback<GetBookmarkFlagResponse> {
+            override fun onFailure(call: Call<GetBookmarkFlagResponse>?, t: Throwable?) {
+            }
+
+            override fun onResponse(call: Call<GetBookmarkFlagResponse>?, response: Response<GetBookmarkFlagResponse>?) {
+                if (response!!.isSuccessful){
+                    if(response.body().message=="1") {  // 즐찾 중이라면 찬 별 표시
+                        nearbyViewHolder.label.setImageResource(R.drawable.market_favorite_favorite_o)
+                    }
+                }
+            }
+        })
 
         var tag : List<String> =nearbyItems[position].tag_name!!.split(",")
 
--- Woong_Android/app/src/main/java/com/woong/woong_android/market/nearby/MarketNearby.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/market/nearby/MarketNearby.kt
@@ -1,5 +1,6 @@
 package com.woong.woong_android.market.nearby
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -12,9 +13,12 @@ import com.woong.woong_android.R
 import com.woong.woong_android.applicationcontroller.ApplicationController
 import com.woong.woong_android.market.adapter.MarketNearbyAdapter
 import com.woong.woong_android.network.NetworkService
+import com.woong.woong_android.seller_market.SellerMarketActivity
 import com.woong.woong_android.seller_market.get.GetNearMarketListResponse
 import com.woong.woong_android.seller_market.get.GetNearMarketListResponseData
+import com.woong.woong_android.woong_marketinfo
 import com.woong.woong_android.woong_usertoken
+import kotlinx.android.synthetic.main.fragment_nearby_market.*
 import kotlinx.android.synthetic.main.fragment_nearby_market.view.*
 
 
@@ -23,12 +27,22 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MarketNearby: Fragment() {
+class MarketNearby: Fragment(),View.OnClickListener {
     lateinit var marketNearbyAdapter: MarketNearbyAdapter
     lateinit var marketNearbyItems : ArrayList<GetNearMarketListResponseData>
     lateinit var networkService: NetworkService
     lateinit var requestManager: RequestManager
+
+    override fun onClick(v: View?) {
+        val intent = Intent(context, SellerMarketActivity::class.java)
+
+        var idx : Int = this.rv_market_mymarket.getChildAdapterPosition(v)
+        woong_marketinfo.market_id = marketNearbyItems[idx].market_id
+        startActivity(intent)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         val v = inflater.inflate(R.layout.fragment_nearby_market,container,false)
         requestManager = Glide.with(this)
         networkService = ApplicationController.instance.networkService
@@ -45,7 +59,7 @@ class MarketNearby: Fragment() {
                 if(response!!.isSuccessful){
                     marketNearbyItems = response.body().data
                     marketNearbyAdapter = MarketNearbyAdapter(marketNearbyItems,requestManager)
-
+                    marketNearbyAdapter.setOnItemClickListener(this@MarketNearby)
                     v.rv_market_mymarket.layoutManager = LinearLayoutManager(context)
                     v.rv_market_mymarket.adapter = marketNearbyAdapter
 
