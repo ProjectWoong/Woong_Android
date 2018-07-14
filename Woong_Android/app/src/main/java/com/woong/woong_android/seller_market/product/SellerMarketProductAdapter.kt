28644597a91043e927c7c374accdfa9764f73bdb
sovovy <sovovy@gmail.com>
2018-07-14 14:30:38 +0900

    마켓 물품 찜하기.....

--- Woong_Android/app/src/main/java/com/woong/woong_android/seller_market/product/SellerMarketProductAdapter.kt
+++ Woong_Android/app/src/main/java/com/woong/woong_android/seller_market/product/SellerMarketProductAdapter.kt
@@ -16,37 +16,21 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
-import android.system.Os.link
-import android.os.AsyncTask
 
 
-
-class SellerMarketProductAdapter(var productItems : ArrayList<GetSellerMarketProductResponseData>,var requestManager: RequestManager) : RecyclerView.Adapter<SellerMarketProductViewHolder>() {
-    private lateinit var onItemClick : View.OnClickListener
-    var favoriteFlags:ArrayList<Int> =ArrayList(Collections.nCopies(itemCount,0))
-    fun setOnItemClickListener(l : View.OnClickListener) {
+class SellerMarketProductAdapter(var productItems: ArrayList<GetSellerMarketProductResponseData>, var requestManager: RequestManager) : RecyclerView.Adapter<SellerMarketProductViewHolder>() {
+    private lateinit var onItemClick: View.OnClickListener
+    var favoriteFlags: ArrayList<Int> = ArrayList()
+    fun setOnItemClickListener(l: View.OnClickListener) {
         onItemClick = l
     }
-    fun a(){
-        var networkService = ApplicationController.instance.networkService
-        for(i in productItems.indices) {
-            val getFavoriteFlag = networkService.getFavoriteFlag(woong_usertoken.user_token, productItems[i].item_id)
-            getFavoriteFlag.enqueue(object : Callback<GetFavoriteFlagResponse> {
-                override fun onFailure(call: Call<GetFavoriteFlagResponse>?, t: Throwable?) {
-                }
 
-                override fun onResponse(call: Call<GetFavoriteFlagResponse>?, response: Response<GetFavoriteFlagResponse>?) {
-                    if (response!!.isSuccessful) {
-                        favoriteFlags[i] = response!!.body().message.toInt()
-                        Log.d("ASDa", favoriteFlags[i].toString())
-                    }
-                }
-            })
-        }
-    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SellerMarketProductViewHolder {
-        val mainView : View = LayoutInflater.from(parent.context).inflate(R.layout.item_sellermarket_product, parent, false)
+        val mainView: View = LayoutInflater.from(parent.context).inflate(R.layout.item_sellermarket_product, parent, false)
         mainView.setOnClickListener(onItemClick)
+        for (i in 1..itemCount) {
+            favoriteFlags.add(0)
+        }
         return SellerMarketProductViewHolder(mainView)
     }
 
@@ -55,52 +39,67 @@ class SellerMarketProductAdapter(var productItems : ArrayList<GetSellerMarketPro
     override fun onBindViewHolder(holder: SellerMarketProductViewHolder, position: Int) {
         var networkService = ApplicationController.instance.networkService
 
-        a()
+        val getFavoriteFlag = networkService.getFavoriteFlag(woong_usertoken.user_token, productItems[position].item_id)
+        getFavoriteFlag.enqueue(object : Callback<GetFavoriteFlagResponse> {
+            override fun onFailure(call: Call<GetFavoriteFlagResponse>?, t: Throwable?) {
+            }
+
+            override fun onResponse(call: Call<GetFavoriteFlagResponse>?, response: Response<GetFavoriteFlagResponse>?) {
+                if (response!!.isSuccessful) {
+                    favoriteFlags[position] = response!!.body().message.toInt()
+                    Log.d("ASDa", favoriteFlags[position].toString())
+                }
+            }
+        })
         requestManager.load(productItems[position].file_key).apply {
             placeholder(R.drawable.flicker).thumbnail(requestManager.load(R.drawable.flicker))
         }.into(holder.productimg)
         holder.marketname.text = productItems[position].market_name
-        holder.productname.text =productItems[position].item_name
+        holder.productname.text = productItems[position].item_name
         holder.cost_per_unit.text = productItems[position].packaging
 
-        Log.d("ASDb",favoriteFlags[position].toString())
-        if (favoriteFlags[position]==1){
-            Log.d("ASDc","ASD")
+        Log.d("ASDb", favoriteFlags[position].toString())
+        if (favoriteFlags[position] == 1) {
+            Log.d("ASDc", "ASD")
             holder.favorite.setImageResource(R.drawable.home_select_category_like1)
             holder.favorite.setOnClickListener {
-                Log.d("asd","aaaaaaa")
+                Log.d("asd", "aaaaaaa")
                 val delFavorite = networkService.delFavorite(woong_usertoken.user_token, productItems[position].item_id)
                 delFavorite.enqueue(object : Callback<PostFavoriteResponse> {
                     override fun onFailure(call: Call<PostFavoriteResponse>?, t: Throwable?) {
-                        Log.d("asd",t.toString())
+                        Log.d("asd", t.toString())
                     }
+
                     override fun onResponse(call: Call<PostFavoriteResponse>?, response: Response<PostFavoriteResponse>?) {
                         if (response!!.isSuccessful) {
-                            Log.d("asd","asdasd")
+                            Log.d("asd", "asdasd")
                             holder.favorite.setImageResource(R.drawable.home_select_category_no_like)
                             favoriteFlags[position] = 0
-                            notifyItemRangeChanged(position,itemCount)
+                            notifyItemRangeChanged(position, itemCount)
                         }
                     }
                 })
             }
-        }else{
-            Log.d("ASDd","ASD")
+        } else {
+            Log.d("ASDd", "ASD")
             holder.favorite.setImageResource(R.drawable.home_select_category_no_like)
             holder.favorite.setOnClickListener {
                 val postFavorite = networkService.postFavorite(woong_usertoken.user_token, productItems[position].item_id)
                 postFavorite.enqueue(object : Callback<PostFavoriteResponse> {
                     override fun onFailure(call: Call<PostFavoriteResponse>?, t: Throwable?) {
                     }
+
                     override fun onResponse(call: Call<PostFavoriteResponse>?, response: Response<PostFavoriteResponse>?) {
                         if (response!!.isSuccessful) {
                             holder.favorite.setImageResource(R.drawable.home_select_category_like1)
                             favoriteFlags[position] = 1
-                            notifyItemRangeChanged(position,itemCount)
+                            notifyItemRangeChanged(position, itemCount)
                         }
                     }
                 })
             }
         }
+
     }
+}
 }
